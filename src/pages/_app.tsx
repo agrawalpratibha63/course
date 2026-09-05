d17b9ddfca58890e1362e2b6091cfa13bbe12b5b
Pratibha Agrawal <agrawalpratibha63@gmail.com>
2026-09-05 11:40:21 +0530

    Add offline course access

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,8 +3,12 @@ import Navbar from "@/Components/Navbar";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { AppProvider } from "@/context/AppContext";
+import { useEffect } from "react";
 
 export default function App({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    if ("serviceWorker" in navigator) navigator.serviceWorker.register("/sw.js").catch(() => undefined);
+  }, []);
   return (
     <AppProvider><div className="bg-white min-h-screen">
       <Navbar />
